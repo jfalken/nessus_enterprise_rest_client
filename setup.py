068cfd7839621625da3cdcf5f949fe9212f4d576
chris sandulow <chris.sandulow@mongodb.com>
2014-11-20 17:18:30 -0500

    setup.py added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='NessusClient',
+    version='0.1',
+    description='python library for Nessus v6 API',
+    author='jfalken',
+    url='https://github.com/jfalken/nessus_enterprise_rest_client',
+    install_requires=['requests'],
+)
